2bdc331ac1ce3576279908687cff2b0e328e4365
Fernando Lira <fernando@datatrust.pe>
2020-06-20 11:44:49 -0500

    view

---
+++ colegio/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='index'),
+    path('ver-asistencias', views.getAsistencias, name='ver_asistencias')
+]
