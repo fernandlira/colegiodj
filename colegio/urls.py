2623ab856f87cb3e6d05df2469ba4eb6a8faec07
Fernando Lira <fernando@datatrust.pe>
2020-06-20 16:07:46 -0500

    new release

--- colegio/urls.py
+++ colegio/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('ver-asistencias', views.getAsistencias, name='ver_asistencias'),
-    path('ver-asistencia/<int:id>', views.getAsistencia, name='ver_asistencia_alumno')
+    path('ver-asistencia/<int:id>', views.getAsistencia, name='ver_asistencia_alumno'),
+    path('<int:id>/comentar', views.makeComentario, name='justificacion_asistencias')
 ]
--- colegio/views.py
+++ colegio/views.py
@@ -16,3 +16,10 @@ def getAsistencia(request, id=1):
     asistencias = Asistencia.objects.filter(alumno=id)
     alumno = Alumno.objects.get(pk=id)
     return render(request,'colegio/asistencia_estudiante.html',{'asistencias':asistencias,'alumno':alumno})
+
+@require_http_methods(["GET","POST"])
+def makeComentario(request, id=1):
+    if request.method == "POST":
+        pass
+    else:
+        return render(request,'colegio/asistencia_estudiante.html',{'asistencias':asistencias,'alumno':alumno})
