9c980fd1c4db7893cf91caae53e0a2c761ac02c7
evavargas <eva.estrella96@gmail.com>
2020-06-20 17:42:35 -0500

    users modified

--- colegio/views.py
+++ colegio/views.py
@@ -11,24 +11,31 @@ def home(request):
 
 def getAsistencias(request):
     alumnos = Alumno.objects.all()
-    return render(request,'colegio/asistencias.html',{'alumnos':alumnos})
+    return render(request, "colegio/asistencias.html", {"alumnos": alumnos})
+
 
 def getAsistencia(request, id=1):
     asistencias = Asistencia.objects.filter(alumno=id)
     alumno = Alumno.objects.get(pk=id)
-    return render(request,'colegio/asistencia_estudiante.html',{'asistencias':asistencias,'alumno':alumno})
+    return render(
+        request,
+        "colegio/asistencia_estudiante.html",
+        {"asistencias": asistencias, "alumno": alumno},
+    )
+
 
 def getNotas(request, id=1):
     notas = Nota.objects.filter(asistencia=id)
-    return render(request,'colegio/notas.html',{'notas':notas})
+    return render(request, "colegio/notas.html", {"notas": notas})
 
-@require_http_methods(["GET","POST"])
+
+@require_http_methods(["GET", "POST"])
 def makeComentario(request, id=1):
     if request.method == "POST":
         form = ComentarioForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('colegio:ver_asistencias')
+            return redirect("colegio:ver_asistencias")
     else:
         form = ComentarioForm
-        return render(request,'colegio/ingresar_comentario.html',{ 'form': form})
+        return render(request, "colegio/ingresar_comentario.html", {"form": form})
